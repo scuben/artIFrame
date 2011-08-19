94815b2c203c50f0cafcc0ae3e94b2f30d7b5a4d
Patrick Landolt <patrick.landolt@artack.ch>
2011-08-19 14:11:47 +0200

    -

--- config/database.sql
+++ config/database.sql
@@ -11,22 +11,19 @@
 -- --------------------------------------------------------
 
 -- 
--- Table `tl_art_iframe`
+-- Table `tl_module`
 -- 
 
-CREATE TABLE `tl_art_iframe` (
-  `id` int(10) unsigned NOT NULL auto_increment,
-  `tstamp` int(10) unsigned NOT NULL default '0',
-  `title` varchar(255) NOT NULL default '',
-  `headline` varchar(255) NOT NULL default '',
-  `jumpTo` int(10) unsigned NOT NULL default '0',
-  `allowComments` char(1) NOT NULL default '',
-  `notify` varchar(32) NOT NULL default '',
-  `sortOrder` varchar(32) NOT NULL default '',
-  `perPage` smallint(5) unsigned NOT NULL default '0',
-  `moderate` char(1) NOT NULL default '',
-  `bbcode` char(1) NOT NULL default '',
-  `requireLogin` char(1) NOT NULL default '',
-  `disableCaptcha` char(1) NOT NULL default '',
+CREATE TABLE `tl_module` (
+  `art_iframe_title` varchar(255) NOT NULL default '',
+  `art_iframe_src` varchar(255) NOT NULL default '',
+  `art_iframe_width` varchar(255) NOT NULL default '',
+  `art_iframe_height` varchar(255) NOT NULL default '',
+  `art_iframe_align` varchar(255) NOT NULL default '',
+  `art_iframe_frameborder` char(1) NOT NULL default '',
+  `art_iframe_longdesc` varchar(255) NOT NULL default '',
+  `art_iframe_name` varchar(255) NOT NULL default '',
+  `art_iframe_scrolling` varchar(255) NOT NULL default '',
+  `art_iframe_src` varchar(255) NOT NULL default '',
   PRIMARY KEY  (`id`)
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8;
