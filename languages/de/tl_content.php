23ab91da9b893c246c8319b3b88cd1a930e18b73
Patrick Landolt <patrick.landolt@artack.ch>
2011-08-23 00:05:14 +0200

    new files

---
+++ languages/de/tl_content.php
@@ -0,0 +1,25 @@
+<?php if (!defined('TL_ROOT')) die('You cannot access this file directly!');
+
+/**
+ *
+ * PHP version 5
+ * @copyright  ARTACK WebLab GmbH - 2011
+ * @author     Patrick Landolt <http://www.artack.ch>
+ * @package    iFrame
+ * @license    LGPL
+ * @filesource
+ */
+
+
+/**
+ * Fields
+ */
+$GLOBALS['TL_LANG']['tl_content']['art_iframe_title']         = array('Titel', 'Der Titel des iFrames.');
+$GLOBALS['TL_LANG']['tl_content']['art_iframe_src']           = array('Source - Link', 'Link welcher im iFrame geladen werden soll.');
+$GLOBALS['TL_LANG']['tl_content']['art_iframe_longdesc']      = array('Beschreibung - Link', 'Link der Beschreibung des iFrames.');
+$GLOBALS['TL_LANG']['tl_content']['art_iframe_width']         = array('Breite', 'Die Breite des iFrames.');
+$GLOBALS['TL_LANG']['tl_content']['art_iframe_height']        = array('Höhe', 'Die Höhe des iFrames.');
+$GLOBALS['TL_LANG']['tl_content']['art_iframe_frameborder']   = array('Rahmen', 'Aktivierung / Deaktivierung des Rahmens des iFrames.');
+$GLOBALS['TL_LANG']['tl_content']['art_iframe_scrolling']     = array('Scrollbalken', 'Aktivierung / Deaktivierung des Scrollbalkens des iFrames.');
+
+?>
